82f4da3ce43937ab31ac508f31052514e0b9c686
isgaroth-pl <isgaroth-pl@users.noreply.github.com>
2021-01-29 14:15:18 +0100

    Addin Unity Project

---
+++ TPPMovementProject/TPPMovementProject/Assets/Scripts/AnimationStateController2D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController2D : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] float _acceleration = 2f;
+    [SerializeField] float _decceleration = 2f;
+    [SerializeField] float maxWalkVelocity = 0.5f;
+    [SerializeField] float maxRunVelocity = 2f;
+    private float _velocityZ = 0f;
+    private float _velocityX = 0f;
+
+
+    void Awake() => animator = GetComponent<Animator>();
+
+    private void Update()
+    {
+        bool forwardPressed = Input.GetKey("w");
+        bool leftPressed = Input.GetKey("a");
+        bool rightPressed = Input.GetKey("d");
+        bool runPressed = Input.GetKey("left shift");
+
+        float currentMaxVelocity = runPressed ? maxRunVelocity : maxWalkVelocity;
+
+        if(forwardPressed && _velocityZ < currentMaxVelocity)
+            _velocityZ += Time.deltaTime * _acceleration;
+        if (leftPressed && _velocityX > -currentMaxVelocity)
+            _velocityX -= Time.deltaTime * _acceleration;
+        if (rightPressed && _velocityX < currentMaxVelocity)
+            _velocityX += Time.deltaTime * _acceleration;
+        if(!forwardPressed && _velocityZ > 0f)
+            _velocityZ -= Time.deltaTime * _decceleration;    
+        if(!forwardPressed && _velocityZ < 0f)
+            _velocityZ = 0f;
+        if (!leftPressed && _velocityX < 0f)
+            _velocityX += Time.deltaTime * _decceleration;
+        if (!rightPressed && _velocityX > 0f)
+            _velocityX -= Time.deltaTime * _decceleration;
+        if (!leftPressed && !rightPressed && _velocityX != 0 && (_velocityX > -0.05f && _velocityX < 0.05f))
+            _velocityX = 0f;
+        if (forwardPressed && runPressed && _velocityZ > currentMaxVelocity)
+            _velocityZ = currentMaxVelocity;
+        else if(forwardPressed && _velocityZ > currentMaxVelocity)
+        {
+            _velocityZ -= Time.deltaTime * _decceleration;
+        }
+
+        animator.SetFloat("Velocity Z", _velocityZ);
+        animator.SetFloat("Velocity X", _velocityX);
+    }
+
+
+}
---
+++ TPPMovementProject/TPPMovementProject/Assets/Scripts/PlayerTPPMovementAnimationController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTPPMovementAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerTPPMovement playerMovement;
+    private int _movementStateHash;
+    private int _airTimeHash;
+    private int _relativeHightHash;
+    private int _isInTheAirHash;
+    private int _isLaunchingHash;
+    private int _isLandingHash;
+    private int _jumpStateHash;
+    private int _fallTimeHash;
+    private int _isAboutToLandHash;
+
+
+    void Awake()
+    {
+        CatchComponentsReferences();
+        SetStringsToHash();           
+    }
+
+    private void Update()
+    {
+        //360 movement
+        animator.SetFloat(_movementStateHash, playerMovement.movementState);
+        //launching, jumping, landing and falling
+        animator.SetBool(_isLaunchingHash, playerMovement.isLaunching);
+        animator.SetBool(_isInTheAirHash, playerMovement.isInTheAir);
+        animator.SetBool(_isLandingHash, playerMovement.isLanding);
+        animator.SetBool(_isAboutToLandHash, playerMovement.isAboutToLand);
+        animator.SetFloat(_airTimeHash, playerMovement.airborneTimer);
+        animator.SetFloat(_relativeHightHash, playerMovement.relativeHight);
+        animator.SetFloat(_jumpStateHash, playerMovement.jumpState);
+        animator.SetFloat(_fallTimeHash, playerMovement.fallTimeAtLanding);
+        //others
+       
+    }
+
+    private void CatchComponentsReferences()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerTPPMovement>();
+    }
+
+    private void SetStringsToHash()
+    {
+        _movementStateHash = Animator.StringToHash("MovementState");
+        _airTimeHash = Animator.StringToHash("AirTime");
+        _relativeHightHash = Animator.StringToHash("RelativeHight");
+        _isInTheAirHash = Animator.StringToHash("IsInTheAir");
+        _isLandingHash = Animator.StringToHash("IsLanding");
+        _isLaunchingHash = Animator.StringToHash("IsLaunching");
+        _jumpStateHash = Animator.StringToHash("JumpState");
+        _fallTimeHash = Animator.StringToHash("FallTime");
+        _isAboutToLandHash = Animator.StringToHash("IsAboutToLand");
+    }
+
+    //public void IsInPivot()
+    //{
+    //    AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+    //}
+
+}
